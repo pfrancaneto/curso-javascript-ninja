07c80208cf576b2fc50d3b1da83dadfb99db0ba3
Pedro França <34045576+pfrancaneto@users.noreply.github.com>
2023-04-17 22:20:14 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myArr = [1, 'teste', true, null, '15'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function arr(myArr) {
+  return myArr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+arr(myArr[2]); // true
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function myFunction(arr, num) {
+  return  arr[num];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myNewArr = [10, 'Pedro', false, undefined, null];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction(myNewArr, 0));
+myFunction(myNewArr, 1));
+myFunction(myNewArr, 2));
+myFunction(myNewArr, 3));
+myFunction(myNewArr, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,12 +55,37 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(livro) {
+  var biblioteca = {
+    ratibum: {
+      quantidadePaginas: 100,
+      autor: 'Kleber',
+      editora: 'Globo',
+    },
+    seculos: {
+      quantidadePaginas: 350,
+      autor: 'João Kleber',
+      editora: 'SBT'
+    },
+    amorTotal: {
+      quantidadePaginas: 50,
+      autor: 'Ítalo',
+      editora: 'Sportv',
+    }
+  }
+  
+  return !livro ? biblioteca : biblioteca[livro];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book(); 
+//{
+ //ratibum: { quantidadePaginas: 100, autor: 'Kleber', editora: 'Globo' },
+ //seculos: { quantidadePaginas: 350, autor: 'João Kleber', editora: 'SBT' },
+ //amorTotal: { quantidadePaginas: 50, autor: 'Ítalo', editora: 'Sportv' }
+//}
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
