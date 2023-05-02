a603fbad7bead56a763a98b76fe4ea096cb9fba7
Pedro França <34045576+pfrancaneto@users.noreply.github.com>
2023-05-02 10:09:35 -0300

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -12,7 +12,12 @@ Invoque a função criada acima, passando dois números que serão somados, e mo
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-console.log('A soma de 2 e 3 é igual a '+ sum(2, 3) + '.') // A soma de 2 e 3 é igual a 5.
+var value1 = 10;
+var value2 = 30;
+
+console.log(
+  "A soma de " + value1 + " e " + value2 + " é igual a " + sum(value1, value2) + "."
+);
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
